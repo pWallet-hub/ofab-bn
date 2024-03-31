52075487197652c3f4ec8c1f51ffbc2cb82330eb
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-03-31 12:55:20 +0200

    Add CORS support to API

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 // app.js
 const express = require('express');
+const cors = require('cors');
 const {run} = require('./db');
 const routes = require('./src/routes/routes');
 
@@ -7,8 +8,23 @@ const app = express();
 
 run();
 
+// Specify the domains that can access your API
+// Specify the domains that can access your API
+const whitelist = ['http://localhost:8080', 'http://localhost:5000'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+// Use CORS with the specified options
+app.use(cors());
+
 app.use(express.json());
 app.use(routes);
-
 
 app.listen(8080, () => console.log('Server started'));
