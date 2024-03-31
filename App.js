0e5abdbc002eb9e6ba67c5dabd8dae4e0d615317
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-03-31 11:15:20 +0200

    Refactor imports and add routes

--- App.js
+++ App.js
@@ -1,18 +1,14 @@
 // app.js
 const express = require('express');
-const run = require('./db');
-const authController = require('./src/controllers/authController');
+const {run} = require('./db');
+const routes = require('./src/routes/routes');
 
 const app = express();
 
 run();
 
 app.use(express.json());
+app.use(routes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-app.post('/signup', authController.signup);
-app.post('/login', authController.login);
 
 app.listen(8080, () => console.log('Server started'));
