a3498bb2420755748e29d529d9e191aa8daedce7
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-04-02 00:20:56 +0200

    Add routes for creating, updating, getting, getting all, and deleting posts

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,16 @@
 // routes.js
 const express = require('express');
 const authController = require('../controllers/authController');
+const postController = require('../controllers/postController');
 
 const router = express.Router();
 
 router.post('/api/v1/signup', authController.signup);
 router.post('/api/v1/login', authController.login);
+router.post('/api/v1/posts', postController.create);
+router.put('/api/v1/posts/:id', postController.update);
+router.get('/api/v1/posts/:id', postController.get);
+router.get('/api/v1/posts', postController.getAll);
+router.delete('/api/v1/posts/:id', postController.delete);
 
 module.exports = router;
