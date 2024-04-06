42d38ac1606e8efe4f68d2b5dfc8f63fd2c83413
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-04-06 09:53:52 +0200

    Add new fields for image description, main photo, and other photos in Post model

--- src/models/post.js
+++ src/models/post.js
@@ -6,6 +6,9 @@ const PostSchema = new mongoose.Schema({
   content: { type: String, required: true },
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   image: { type: String }, // New field for image
+  imageDescription: { type: String }, // New field for image description
+  mainPhoto: { type: String }, // New field for main photo of story
+  otherPhotos: [{ type: String }], // New field for other photos of story
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
 });
