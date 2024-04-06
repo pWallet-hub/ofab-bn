454f01d7da6b88c09ef6bb747bbdc74044afcf6b
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-04-06 09:54:03 +0200

    Update post creation route to allow uploading multiple photos

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.post('/api/v1/signup', authController.signup);
 router.post('/api/v1/login', authController.login);
-router.post('/api/v1/posts', upload.single('image'), postController.create);
+router.post('/api/v1/posts', upload.fields([{ name: 'mainPhoto', maxCount: 1 }, { name: 'otherPhotos', maxCount: 10 }]), postController.create);
 router.put('/api/v1/posts/:id', postController.update);
 router.get('/api/v1/posts/:id', postController.get);
 router.get('/api/v1/posts', postController.getAll);
