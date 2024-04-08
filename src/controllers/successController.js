d27719981f05607da59c22d767dbf807859e229c
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-04-08 16:47:31 +0200

    Add successController.js with create and getAll functions

---
+++ src/controllers/successController.js
@@ -0,0 +1,39 @@
+// controllers/SuccessStoryController.js
+const { getDb } = require('../../db');
+const SuccessStory = require('../models/SuccessStory');
+const mongodb = require('mongodb');
+const cloudinary = require('cloudinary').v2;
+
+
+
+exports.create = async (req, res) => {
+  try {
+    const db = getDb();
+    const successStoriesCollection = db.collection('successStories');
+
+    const image = await cloudinary.uploader.upload(req.file.path);
+
+    const successStory = new SuccessStory({
+      name: req.body.name,
+      story: req.body.story,
+      imageUrl: image.secure_url, // this is the link to the image stored in Cloudinary
+      createdAt: Date.now(),
+      updatedAt: Date.now()
+    });
+
+    await successStoriesCollection.insertOne(successStory);
+    res.status(201).send(successStory);
+  } catch (error) {
+    res.status(400).send(error);
+    console.log(error);
+  }
+};
+
+exports.getAll = async (req, res) => {
+  try {
+    const successStories = await SuccessStory.find();
+    res.status(200).send(successStories);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
