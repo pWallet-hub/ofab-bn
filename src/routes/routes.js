268a21f0719f47a73f0cb7b0c63826371748572d
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-04-08 09:05:53 +0200

    Add teamController.js with create, getAll, getOne, update, and delete functions

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const authController = require('../controllers/authController');
 const postController = require('../controllers/postController');
+const teamController = require('../controllers/teamController');
 const upload = require('../middlewires/upload');
 
 const router = express.Router();
@@ -13,5 +14,10 @@ router.put('/api/v1/posts/:id', postController.update);
 router.get('/api/v1/posts/:id', postController.get);
 router.get('/api/v1/posts', postController.getAll);
 router.delete('/api/v1/posts/:id', postController.delete);
+router.post('/api/v1/teams', upload.single('picture'), teamController.create);
+router.get('/api/v1/teams', teamController.getAll);
+router.get('/api/v1/teams/:id', teamController.getOne);
+router.put('/api/v1/teams/:id', teamController.update);
+router.delete('/api/v1/teams/:id', teamController.delete);
 
 module.exports = router;
