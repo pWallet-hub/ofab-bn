71423668f5112fb63f6302a023326f084d74e9ef
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-05-01 12:47:48 +0200

    Add capacity building routes to routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,7 @@ const postController = require('../controllers/postController');
 const teamController = require('../controllers/teamController');
 const galleryController = require('../controllers/galleryController');
 const successController = require('../controllers/successController');
+const capacityController = require('../controllers/capacityBuildingController');
 const upload = require('../middlewires/upload');
 
 const router = express.Router();
@@ -32,6 +33,12 @@ router.delete('/api/v1/galleries/:id', galleryController.delete);
 
 router.post('/api/v1/success-stories', upload.single('image'), successController.create);
 router.get('/api/v1/success-stories', successController.getAll);
+
+router.post('/api/v1/capacity',upload.fields([{ name: 'image', maxCount: 10 }, { name: 'cover', maxCount: 10 }]),capacityController.create);
+router.get('/api/v1/capacity', capacityController.getAll);
+router.get('/api/v1/capacity/:_id', capacityController.getOne);
+router.delete('/api/v1/capacity/:_id', capacityController.delete);
+
 
 
 module.exports = router;
