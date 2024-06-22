48a18e5e9d58587f0175f43088b14e025513d507
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-06-22 14:18:19 +0200

    feat: Add mainPhoto field to Post model and update create function in postController.js

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,4 +1,3 @@
-// controllers/PostController.js
 const { getDb } = require('../../db');
 const Post = require('../models/post');
 const mongodb = require('mongodb');
@@ -14,21 +13,20 @@ exports.create = async (req, res) => {
     const postsCollection = db.collection('posts');
 
     const mainPhotoResult = await cloudinary.uploader.upload(req.files.mainPhoto[0].path);
-    const otherPhotoResults = await Promise.all(req.files.otherPhotos.map(file => cloudinary.uploader.upload(file.path)));
+    //const otherPhotoResults = await Promise.all(req.files.otherPhotos.map(file => cloudinary.uploader.upload(file.path)));
 
     const post = new Post({
       title: req.body.title,
-      content: req.body.content,
       author: req.body.author,
-      image: mainPhotoResult.secure_url, // this is the link to the main photo stored in Cloudinary
-      imageDescription: req.body.imageDescription, // image description
-      mainPhoto: mainPhotoResult.secure_url, // main photo of the story
-      otherPhotos: otherPhotoResults.map(result => result.secure_url), // other photos of the story
-      createdAt: Date.now(),
-      updatedAt: Date.now()
+      publicationDate: req.body.publicationDate,
+      link: req.body.link,
+      activitySource: req.body.activitySource,
+      articleSummary: req.body.articleSummary,
+      mainPhoto: mainPhotoResult.secure_url,
     });
 
     await postsCollection.insertOne(post);
+    console.log('Post created:', post);
     res.status(201).send(post);
   } catch (error) {
     console.log('Error message:', error.message);
@@ -63,26 +61,15 @@ exports.get = async (req, res) => {
     const db = getDb();
     const postsCollection = db.collection('posts');
 
-    const post = await postsCollection.aggregate([
-      { $match: { _id: new mongodb.ObjectId(req.params.id) } },
-      {
-        $lookup: {
-          from: 'users',
-          localField: 'author',
-          foreignField: '_id',
-          as: 'author'
-        }
-      },
-      { $unwind: '$author' },
-      { $project: { 'author.password': 0 } }
-    ]).toArray();
+    const post = await postsCollection.findOne({ _id: new mongodb.ObjectId(req.params.id) });
 
-    if (!post.length) {
+    if (!post) {
       return res.status(404).send();
     }
-    res.send(post[0]);
+    res.send(post);
   } catch (error) {
     res.status(500).send(error);
+    console.log(error);
   }
 };
 
@@ -91,20 +78,10 @@ exports.getAll = async (req, res) => {
     const db = getDb();
     const postsCollection = db.collection('posts');
 
-    const posts = await postsCollection.aggregate([
-      {
-        $lookup: {
-          from: 'users',
-          localField: 'author',
-          foreignField: '_id',
-          as: 'author'
-        }
-      },
-      { $unwind: '$author' },
-      { $project: { 'author.password': 0 } }
-    ]).toArray();
+    const posts = await postsCollection.find({}).toArray();
     res.send(posts);
   } catch (error) {
+    console.error(error); // Log the error for debugging
     res.status(500).send(error);
   }
 };
